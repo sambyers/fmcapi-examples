98c6c469104bbe80d6433e724446c4c95f9a3fae
sam byers <samwbyers@gmail.com>
2017-09-28 15:41:50 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # fmcapi-examples
-Example Scripts using the [FMCAPI](https://github.com/daxm/fmcapi)
+Example Scripts using the [fmcapi](https://github.com/daxm/fmcapi)
+
+## Getting started with rmv_objects
+rmv_objects removes objects from the FMC with the fmcapi library. You can specify the type of object you want to remove and a regular expression to match on object names you want to delete. This script will not delete objects that are in use.
+
+- Install [python](python.org)
+- pip install -r requirements.txt
+- python rmv_objects.py server username password object_type regex
+
+## Usage
+
+python rmv_objects.py -h
+usage: rmv_objects.py [-h] server username password object_type regex
+
+Get arguments for rmv_objects script.
+
+positional arguments:
+  server       IP or DNS of the FMC Server
+  username     Username for FMC.
+  password     Password for FMC.
+  object_type  Object type to remove. protocolport and ipnetwork is supported
+               today.
+  regex        The regular expression to match objects to remove. Use double
+               quotes around the pattern.
---
+++ rmv_objects.py
@@ -0,0 +1,79 @@
+from fmcapi import *
+from re import search
+from argparse import ArgumentParser
+
+def get_args():
+    '''
+    Get args from CLI.
+    '''
+    parser = ArgumentParser(description='Get arguments for rmv_objects script.')
+    parser.add_argument('server', type=str, help='IP or DNS of the FMC Server')
+    parser.add_argument('username', type=str, help='Username for FMC.')
+    parser.add_argument('password', type=str, help='Password for FMC.')
+    parser.add_argument('object_type', type=str, help='Object type to remove. protocolport and ipnetwork is supported today.')
+    parser.add_argument('regex', type=str, help='The regular expression to match objects to remove. Use double quotes around the pattern.')
+
+    args = parser.parse_args()
+    return args
+
+
+def del_obj(obj1, result, regex):
+
+    print('Let\'s delete some stuff! -->')
+
+    del_count = 0
+
+    for k in result['items']:
+        
+        if search(regex, k['name']):
+            print("Deleting {}".format(k['name']))
+            try:
+                obj1.delete(id=k['id'])
+                del_count = del_count + 1
+            except TypeError:
+                print("We can't delete objects that are in use.")
+    
+    if del_count:
+        return del_count
+
+
+def main():
+
+    args = get_args()
+    fmc_server = args.server
+    username = args.username
+    password = args.password
+    object_type = args.object_type
+    regex = args.regex
+
+    object_type.lower()
+    
+    implemented_objects = ('protocolport', 'ipnetwork', 'networkgroup')
+
+    if object_type in implemented_objects:
+
+        with FMC(host=fmc_server, username=username, password=password, autodeploy=False) as fmc1:
+
+            if object_type == 'protocolport':
+                obj1 = ProtocolPort(fmc=fmc1)
+                result = obj1.get()
+                del_count = del_obj(obj1, result, regex)
+
+            elif object_type == 'ipnetwork':
+                obj1 = IPNetwork(fmc=fmc1)
+                result = obj1.get()
+                del_count = del_obj(obj1, result, regex)
+
+            elif object_type == 'networkgroup':
+                obj1 = NetworkGroup(fmc=fmc1)
+                result = obj1.get()
+                del_count = del_obj(obj1, result, regex)
+
+            print("Deleted {} {} object(s).".format(del_count, object_type))
+
+    else:
+        print('Not an implemented object type.')
+
+
+if __name__ == '__main__':
+    main()
